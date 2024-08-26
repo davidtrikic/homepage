d3d8962b0e01e5085d53f9797afc95f809d2b6b3
david <david@DELL.local>
2024-08-26 16:54:15 +0200

    fix date function

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-let searchBtn = document.getElementById('search-btn');
-let searchForm = document.getElementById('search-form');
+const searchBtn = document.getElementById('search-btn');
+const searchForm = document.getElementById('search-form');
+const dateBox = document.getElementById('date');
 
+
+// Reset search input 
 resetInput = () => {
   searchForm.reset();
 }
 
-// searchBtn.addEventListener('blur', resetInput);
+// searchBtn.addEventListener('blur', resetInput); -> optional
 window.addEventListener('load', resetInput);
+
+// Sub, 6.04.
+
+//Date function
+const date = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let month = date.getMonth() + 1;
+dateBox.innerHTML = days[date.getDay()] + ", " + date.getDate() + ". " + month + ".";
+
